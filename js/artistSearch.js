7592e2a0e3c57825fd843018c624ceaeb69414b6
Andre Giron <agiron123@gmail.com>
2015-04-18 01:03:25 -0400

    Artist graph now working. Going to deploy to Heroku shortly

--- js/artistSearch.js
+++ js/artistSearch.js
@@ -1,21 +1,57 @@
 /**
  * Created by andre on 4/17/15.
  */
-// find template and compile it
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
 
-var templateSource = document.getElementById('results-template').innerHTML,
-    template = Handlebars.compile(templateSource),
-    resultsPlaceholder = document.getElementById('results');
+// create an array with nodes
+var nodes = new vis.DataSet();
+var edges = new vis.DataSet();
+// create an array with edges
+
+// create a network
+var container = document.getElementById('visualization');
+var data = {
+    nodes: nodes,
+    edges: edges
+};
+
+function onSelect (properties) {
+    //alert('selected nodes: ' + properties.nodes);
+    var artistId = properties.nodes[0];
+    fetchRelatedArtists(artistId);
+}
+
+// add event listener
+var network = new vis.Network(container, data, {});
+network.on('select', onSelect);
 
 var fetchRelatedArtists = function (artistId, callback) {
     $.ajax({
         url: 'https://api.spotify.com/v1/artists/' + artistId + '/related-artists',
         success: function (response) {
-            callback(response);
+
+            var relatedArtists = response;
+
+            //resultsPlaceholder.innerHTML = template(relatedArtists);
+            console.log("Length: " + relatedArtists.length);
+            for(var i = 0; i < relatedArtists.artists.length; i++)
+            {
+                console.log(relatedArtists.artists[i].name);
+                console.log(relatedArtists.artists[i].id);
+                var artistedge = {id: relatedArtists.artists[i].id, label: relatedArtists.artists[i].name};
+                var name = artistedge.label;
+
+                if(nodes.get(artistedge.id) == null){
+                    nodes.add(artistedge);
+                    edges.add({from: artistId, to: artistedge.id});
+                }
+            }
+
+            network.redraw();
+            //callback(response);
         }
     });
 };
@@ -29,7 +65,9 @@ var searchArtist = function (query, callback) {
         },
         success: function (response) {
             var item = response.artists.items[0];
-            callback(item.id, item.name);
+            var parent = {id: item.id, label: item.name };
+            nodes.add(parent);
+            //callback(item.id, item.name);
         }
     });
 };
@@ -37,12 +75,35 @@ var searchArtist = function (query, callback) {
 document.getElementById('search-form').addEventListener('submit', function (e) {
     e.preventDefault();
     var artistName = document.getElementById('query').value;
+    searchArtist(artistName, function(){});
+
+    /*
     searchArtist(artistName, function(id, foundName) {
+        var parent = {id: id, label: foundName };
+        nodes.add(parent);
+
+        //nodes.onclick
+
         fetchRelatedArtists(id, function(relatedArtists) {
+
             relatedArtists.artistName = foundName;
-            resultsPlaceholder.innerHTML = template(relatedArtists);
-        });
-    });
-}, false);
+
+            //resultsPlaceholder.innerHTML = template(relatedArtists);
+            console.log("Length: " + relatedArtists.length);
+            for(var i = 0; i < relatedArtists.artists.length; i++)
+            {
+                console.log(relatedArtists.artists[i].name);
+                console.log(relatedArtists.artists[i].id);
+                var artistedge = {id: relatedArtists.artists[i].id, label: relatedArtists.artists[i].name};
+                var name = artistedge.label;
+                artistedge.addEventListener("select", searchArtist(name),function(){});
+                nodes.add(artistedge);
+                edges.add({from: parent.id, to: artistedge.id});
+            }
+
+            network.redraw();
+        }); */
+
+    },false);
 
 });
